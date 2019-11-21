c73408f302deadbf61c559e5ca5e41760d2c9e6f
DoberWho <jorge.parada@ctbarbanza.com>
2019-11-21 13:10:44 +0100

    Quitado Menu

--- src/main/java/com/mitienda/spring/StartApplication.java
+++ src/main/java/com/mitienda/spring/StartApplication.java
@@ -1,19 +1,13 @@
 package com.mitienda.spring;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.mitienda.spring.menu.menuPrincipal;
-import com.mitienda.spring.models.Categoria;
-import com.mitienda.spring.repositories.CategoriasRepository;
-
 @SpringBootApplication
-public class StartApplication extends menuPrincipal implements CommandLineRunner {
+public class StartApplication implements CommandLineRunner {
 
 	private static final Logger log = LoggerFactory.getLogger(StartApplication.class);
 
@@ -28,9 +22,7 @@ public class StartApplication extends menuPrincipal implements CommandLineRunner
 		/**
 		 * Aqui controlar los Menus y el cambio entre ellos. Y En cada menu; usar su
 		 * respectivo Controller y Repository
-		 */ 
-
-		menuPrincipal.iniciaMenu();
+		 */
 
 	}
 
