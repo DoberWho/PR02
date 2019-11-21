9b8fb47d94846f48992ba469366b76a02364a3d1
DoberWho <jorge.parada@ctbarbanza.com>
2019-11-21 12:54:57 +0100

    remove SQLITE

--- src/main/java/com/mitienda/spring/repositories/CategoriasRepository.java
+++ src/main/java/com/mitienda/spring/repositories/CategoriasRepository.java
@@ -1,9 +1,11 @@
 package com.mitienda.spring.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.mitienda.spring.models.Categoria;
 
+@Repository
 public interface CategoriasRepository extends CrudRepository<Categoria, Long> {
 
 }
